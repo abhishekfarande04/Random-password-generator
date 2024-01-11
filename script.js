bb42feb293e3df1f0e7e4578c8a6e46d6beb0dc6
Abhishek Farande <farandeabhishek04@gmail.com>
2024-01-11 23:45:34 +0530

    JS file

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputSlider=document.querySelector("[data-lengthSlider]")
+const lengthDisplay=document.querySelector("[data-lengthNumber]")
+
+const passwodDisplay=document.querySelector("data-passwordDisplay");
+co
