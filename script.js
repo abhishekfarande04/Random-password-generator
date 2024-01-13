76d7eae257cbbf59e0474e5ca6db4c97d9d3f431
Abhishek Farande <farandeabhishek04@gmail.com>
2024-01-14 02:19:05 +0530

    added logic for Slider and password generation

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const inputSlider=document.querySelector("[data-lengthSlider]")
-const lengthDisplay=document.querySelector("[data-lengthNumber]")
+// fetching all the elements required for functionality
+const inputSlider=document.querySelector("[data-lengthSlider]");
+const lengthDisplay=document.querySelector("[data-lengthNumber]");
+const passwordDisplay=document.querySelector("data-passwordDisplay");
+const copyBtn=document.querySelector("[data-copy]");
+const copyMsg=document.querySelector("[data-copyMsg]");
+const uppercaseCheck=document.querySelector("[#uppercase]");
+const lowercaseCheck=document.querySelector("[#lowercase]");
+const numbersCheck=document.querySelector("[#numbers]");
+const symbolsCheck=document.querySelector("[#symbols]");
+const indicator=document.querySelector("[data-indicator]");
+const generateBtn=document.querySelector(".generateBtn");
+const allCheckbox=document.querySelector("input[type=checkbox]");
 
-const passwodDisplay=document.querySelector("data-passwordDisplay");
-co
+let password="";
+let passwordLength=10;
+let checkCount=1;
+// ste strength circle color to grey 
+// lets do list of function 
+1. copy 
+2. to handle slider 
+3. we use listener to generate password 
+4. color set for strength
+5. function to generate Random integer
+6. get random integer, getRandomupperCase,getRandomlowerCase, getRandomSymbol, calculate 
